f4be0bdd785c75863ee93bc5aa32e5a12584abc3
Siromakha3000 <Siromakha3000@gmail.com>
2025-07-17 10:11:53 -0400

    task complete

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,21 +1,42 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
+    name: 'cart',
+    initialState: {
+        items: [], // Initialize items as an empty array
     },
-    removeItem: (state, action) => {
+    reducers: {
+        addItem: (state, action) => {
+            const { name, image, cost } = action.payload;
+            const item = state.items
+                .find((item) => action.payload.name === item.name);
+            if (item) {
+                item.quantity++;
+            } else {
+                state.items.push({ name, image, cost, quantity: 1 });
+            }
+        },
+        removeItem: (state, action) => {
+            const { name, image, cost } = action.payload;
+            const item = state.items
+                .find((item) => action.payload.name === item.name);
+            if (item.quantity <= 1) {
+                state.items.pop(item);
+            } else {
+                item.quantity--;
+            }
+        },
+        updateQuantity: (state, action) => {
+            const item = state.items
+                .find((item) => action.payload.name === item.name);
+            if (item) {
+                item.quantity = action.payload.quantity;
+                if (item.quantity <= 0) {
+                    state.items.pop(item);
+                }
+            }
+        },
     },
-    updateQuantity: (state, action) => {
-
-    
-    },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
